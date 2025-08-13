d6c8ce940461d9d596cd91a3b16fdf57f5ba729d
Joe Corall <jjc223@lehigh.edu>
2025-08-13 13:06:14 -0400

    Ensure rel key exists (#1083)

--- src/Flysystem/Adapter/FedoraAdapter.php
+++ src/Flysystem/Adapter/FedoraAdapter.php
@@ -189,7 +189,7 @@ class FedoraAdapter implements AdapterInterface {
     $links = Header::parse($response->getHeader('Link'));
 
     foreach ($links as $link) {
-      if ($link['rel'] == 'type' && $link[0] == '<http://www.w3.org/ns/ldp#NonRDFSource>') {
+      if (isset($link['rel']) && $link['rel'] == 'type' && $link[0] == '<http://www.w3.org/ns/ldp#NonRDFSource>') {
         $type = 'file';
         break;
       }
